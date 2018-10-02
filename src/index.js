bffb30256cd4fa08db127f1a6ba5af7888da192c
Mannuru <arunm057@gmail.com>
2018-10-02 08:13:37 -0700

    Commiting changes of conigs tried
    making changes on configs

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ import registerServiceWorker from './registerServiceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import { BrowserRouter }  from 'react-router-dom';
+import AWS from 'aws-sdk';
+
+AWS.config.update({
+	accessKeyId: 'AKIAJJJS7D2NMVYOITMQ',
+	secretAccessKey:'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'
+});
+
+AWS.config.region = 'us-east-1';
+
+
 
 ReactDOM.render(
   <BrowserRouter>
